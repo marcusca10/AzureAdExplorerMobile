2c59474321d3ef800c4b27f8bdcf4742ce21a359
Marcus Carvalho <marcusca@microsoft.com>
2021-11-03 14:24:25 +0100

    Initial commit

---
+++ AzureAdExplorerMobile/AzureAdExplorerMobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AzureAdExplorerMobile.Services;
+using AzureAdExplorerMobile.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AzureAdExplorerMobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AzureAdExplorerMobile/AzureAdExplorerMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AzureAdExplorerMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AzureAdExplorerMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
